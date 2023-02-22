4ce3307b9639d29a3dfc76a78c7e19d7b8eedfc8
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-02-22 11:31:50 -0500

    Least squares polynomial

---
+++ src/curve_fitting.c
@@ -0,0 +1,39 @@
+#include <math.h>
+#include "curve_fitting.h"
+
+void leastSquaresPolynomial(Point* points, int numPoints, int degree, Vector results) {
+    int polynomialLength = degree + 1;
+    double mainMatrixArray[numPoints * polynomialLength];
+    Matrix mainMatrix = { numPoints, polynomialLength, mainMatrixArray };
+
+    double bArray[numPoints];
+    Vector b = { numPoints, bArray };
+
+    for(int i = 0; i < numPoints; i++) {
+        Point point = points[i];
+        b.elements[i] = point.y;
+        for(int j = 0; j < polynomialLength; j++) {
+            *addressAt(&mainMatrix, i, j) = pow(point.x, j);
+        }
+    }
+
+    double transposedMatrixArray[numPoints * polynomialLength];
+    Matrix transposedMatrix = { polynomialLength, numPoints, transposedMatrixArray };
+    transpose(&mainMatrix, &transposedMatrix);
+
+    // A^T b - A^T A x_ = 0
+    // A^T b = A^T A x_
+    double matrixProductArray[polynomialLength * polynomialLength];
+    Matrix matrixProduct = { polynomialLength, polynomialLength, matrixProductArray };
+    mmProduct(&transposedMatrix, &mainMatrix, &matrixProduct);
+
+    double inversedProductArray[polynomialLength * polynomialLength];
+    Matrix inversedProduct = { polynomialLength, polynomialLength, inversedProductArray };
+    inverse(&matrixProduct, &inversedProduct); // (A^T A)^-1
+
+    double transposedTimesBArray[polynomialLength];
+    Vector transposedTimesB = { polynomialLength, transposedTimesBArray };
+    mvProduct(&transposedMatrix, &b, &transposedTimesB); // A^T b
+    
+    mvProduct(&inversedProduct, &transposedTimesB, &results); // (A^T A)^-1 (A^T) b = x_
+}
---
+++ src/curve_fitting.h
@@ -0,0 +1,4 @@
+#include "matrix.h"
+#include "polynomial.h"
+
+void leastSquaresPolynomial(Point* points, int numPoints, int degree, Vector results);
