9c9ef91f73517e912e10e4678319b64f7c61adf7
kevinMEH <71391217+kevinMEH@users.noreply.github.com>
2023-03-16 20:27:33 -0400

    Xorshift RNG

---
+++ src/random.c
@@ -0,0 +1,23 @@
+#include "random.h"
+
+static XorshiftState state;
+
+void setXorshiftState(uint64_t first, uint64_t second) {
+    state.x[0] = first;
+    state.x[1] = second;
+}
+
+uint64_t xorshift() {
+    uint64_t t = state.x[0];
+    uint64_t s = state.x[1];
+    state.x[0] = s;
+    t ^= t << 23;
+    t ^= t >> 18;
+    t ^= s ^ (s >> 5);
+    state.x[1] = t;
+    return t + s;
+}
+
+double xorshiftDouble() {
+    return (double) xorshift() / (0x8000000000000000u * 2.0);
+}
